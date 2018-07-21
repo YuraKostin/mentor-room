e824fffa20eeb988d8e5d6b633c9292155b6cd8e
Yuri Kostin <yurakostinm@yandex.ru>
2018-07-21 22:07:31 +0300

    Add prompt calculator part 3. Fix error in prompt calculator answer part 2

--- homework/js/prompt-calculator/prompt-calculator-3/answer/index.js
+++ homework/js/prompt-calculator/prompt-calculator-3/answer/index.js
@@ -33,17 +33,43 @@ const div = (a, b, invalidArgumentMessage) => {
     throw new Error(`[div]: ${invalidArgumentMessage}`);
 };
 
-const operation = prompt('Type operation (+, -, *, /)');
-const argument1 = Number(prompt('Type first argument'));
-const argument2 = Number(prompt('Type second argument'));
+const actions = '+-*/';
 
-const operations = {
-    '+': add,
-    '-': sub,
-    '*': mul,
-    '/': div,
+const isInvalidAction = string => {
+    return !string || string.length !== 1 || !actions.includes(string);
 };
 
-const result = operations[operation](argument1, argument2, ERROR_MESSAGE_NUMBER_EXPECTED);
+const runCalculator = () => {
+    const operations = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div,
+    };
 
-alert(result);
+    let operation;
+
+    while (isInvalidAction(operation)) {
+        operation = prompt('Type operation (+, -, *, /)');
+    }
+
+    let argument1;
+
+    while (isNaN(argument1)) {
+        argument1 = Number(prompt('Type first argument'));
+    }
+
+    let argument2;
+
+    while (isNaN(argument2)) {
+        argument2 = Number(prompt('Type second argument'));
+    }
+
+    const result = operations[operation](argument1, argument2, ERROR_MESSAGE_NUMBER_EXPECTED);
+
+    alert(result);
+
+    if (confirm('Do you want to calc anything else?')) {
+        runCalculator();
+    }
+};
